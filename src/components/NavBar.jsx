a9f55d11ccc7c882fa51c0a788ddafd3213447fd
David Berenguel <davidberenguel13@gmail.com>
2024-05-21 20:46:46 +0100

    Criado e preenchido algum conteudo no Main e na NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+export default function NavBar() {
+    
+    return(
+        <nav>
+            <ul>
+                <li><a>David Berenguel</a></li>
+                <li><a>About</a></li>
+                <li><a>Projetos</a></li>
+                <li><a>Instagram</a></li>
+                <li><a>Discord</a></li>
+                <li><a>LinkedIn</a></li>
+                <li><a>GitHub</a></li>
+                <li>
+                    Linguagem
+                    <ul>
+                        <li>Português</li>
+                        <li>Inglês</li>
+                    </ul>
+                </li>
+                <li>
+                    Tema
+                    <ul>
+                        <li>Claro</li>
+                        <li>Escuro</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    )
+}
