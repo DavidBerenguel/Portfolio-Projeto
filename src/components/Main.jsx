aeed738ae04690b9b4f19398ca4b96f524fff78c
David Berenguel <davidberenguel13@gmail.com>
2024-05-23 21:56:39 +0100

    Diretorio apagado, foi criado na zona errada

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,28 @@
 export default function Main() {
     return(
         <main>
-            <h1>Olá Mundo!</h1>
-            <h2>Sou o David Berenguel</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed efficitur augue. Etiam et viverra ligula. Praesent cursus laoreet tincidunt. Praesent accumsan non felis vel rutrum. Integer non neque enim. Sed sed nibh vitae diam dignissim rutrum. Curabitur sagittis tortor ac enim commodo rutrum. In hac habitasse platea dictumst. Nullam semper auctor tincidunt. Aliquam lobortis ut metus fermentum aliquet. Pellentesque vel tortor pretium ante blandit imperdiet. Fusce varius ultricies ligula, id consequat justo ultrices ut. Sed congue, odio aliquam vulputate pretium, odio dui placerat lorem, quis malesuada ex leo ut massa. Suspendisse pretium nunc nisi, a ultrices nisi lacinia non. Integer malesuada sapien et dui auctor, laoreet facilisis elit feugiat. Morbi luctus metus vitae efficitur efficitur.
-            </p>
+            <div>
+                <h1>Bem vindo ao meu Portfolio!</h1>
+                <h2>Sou o David Berenguel</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed efficitur augue. Etiam et viverra ligula. Praesent cursus laoreet tincidunt. Praesent accumsan non felis vel rutrum. Integer non neque enim. Sed sed nibh vitae diam dignissim rutrum. Curabitur sagittis tortor ac enim commodo rutrum. In hac habitasse platea dictumst. Nullam semper auctor tincidunt. Aliquam lobortis ut metus fermentum aliquet. Pellentesque vel tortor pretium ante blandit imperdiet. Fusce varius ultricies ligula, id consequat justo ultrices ut. Sed congue, odio aliquam vulputate pretium, odio dui placerat lorem, quis malesuada ex leo ut massa. Suspendisse pretium nunc nisi, a ultrices nisi lacinia non. Integer malesuada sapien et dui auctor, laoreet facilisis elit feugiat. Morbi luctus metus vitae efficitur efficitur.
+                </p>
+            </div>
+            <div>
+                <h2>Tecnical Skills</h2>
+                <p>
+                    HTML
+                    <img scr ="" alt="" />
+                </p>
+                <p>
+                    CSS
+                    <img scr ="" alt="" />
+                </p>
+                <p>
+                    Java Script
+                    <img scr ="" alt="" />
+                </p>
+            </div>
         </main>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,22 @@
+
+
 export default function NavBar() {
     
     return(
         <nav>
-            <ul>
-                <li><a>David Berenguel</a></li>
-                <li><a>About</a></li>
-                <li><a>Projetos</a></li>
-                <li><a>Instagram</a></li>
-                <li><a>Discord</a></li>
-                <li><a>LinkedIn</a></li>
-                <li><a>GitHub</a></li>
+            <a className="logo" href="/" >Portfolio.</a>
+            <ul className="navLink" >
+                <li><a href="/" >Home</a></li>
+                <li><a href="" >Skills</a></li>
+                <li><a href="" >Projeto</a></li>
+                <li><a href="" >LinkedIn</a></li>
+                <li><a href="" >Instagram</a></li>
+                <li><a href="" >Facebook</a></li>
                 <li>
                     Linguagem
                     <ul>
                         <li>Português</li>
                         <li>Inglês</li>
-                    </ul>
-                </li>
-                <li>
-                    Tema
-                    <ul>
-                        <li>Claro</li>
-                        <li>Escuro</li>
                     </ul>
                 </li>
             </ul>
